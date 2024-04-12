3449a85f54affe369e848011e17b7c85eeb88827
hit9 <hit9@icloud.com>
2024-04-12 13:25:34 +0800

    Add selector unittest for priority

--- tests/types.h
+++ tests/types.h
@@ -6,20 +6,33 @@ struct Blackboard {
   int counterA = 0;
   int counterB = 0;
   int counterE = 0;
+  int counterG = 0;
+  int counterH = 0;
+  int counterI = 0;
 
   // Commands for every Action/Condition.
   bt::Status shouldA = bt::Status::RUNNING;
   bt::Status shouldB = bt::Status::RUNNING;
   bt::Status shouldE = bt::Status::RUNNING;
+  bt::Status shouldG = bt::Status::RUNNING;
+  bt::Status shouldH = bt::Status::RUNNING;
+  bt::Status shouldI = bt::Status::RUNNING;
 
   bool shouldC = false;
   bool shouldD = false;
   bool shouldF = false;
 
+  uint shouldPriorityG = 0;
+  uint shouldPriorityH = 0;
+  uint shouldPriorityI = 0;
+
   // Status snapshots for every Action.
   bt::Status statusA = bt::Status::UNDEFINED;
   bt::Status statusB = bt::Status::UNDEFINED;
   bt::Status statusE = bt::Status::UNDEFINED;
+  bt::Status statusG = bt::Status::UNDEFINED;
+  bt::Status statusH = bt::Status::UNDEFINED;
+  bt::Status statusI = bt::Status::UNDEFINED;
 };
 
 class A : public bt::Action {
@@ -73,3 +86,41 @@ class F : public bt::Condition {
   }
 };
 
+class G : public bt::Action {
+ public:
+  bt::Status Update(const bt::Context& ctx) override {
+    auto bb = std::any_cast<std::shared_ptr<Blackboard>>(ctx.data);
+    bb->counterG++;
+    return bb->statusG = bb->shouldG;
+  }
+  uint Priority(const bt::Context& ctx) const override {
+    auto bb = std::any_cast<std::shared_ptr<Blackboard>>(ctx.data);
+    return bb->shouldPriorityG;
+  }
+};
+
+class H : public bt::Action {
+ public:
+  bt::Status Update(const bt::Context& ctx) override {
+    auto bb = std::any_cast<std::shared_ptr<Blackboard>>(ctx.data);
+    bb->counterH++;
+    return bb->statusH = bb->shouldH;
+  }
+  uint Priority(const bt::Context& ctx) const override {
+    auto bb = std::any_cast<std::shared_ptr<Blackboard>>(ctx.data);
+    return bb->shouldPriorityH;
+  }
+};
+
+class I : public bt::Action {
+ public:
+  bt::Status Update(const bt::Context& ctx) override {
+    auto bb = std::any_cast<std::shared_ptr<Blackboard>>(ctx.data);
+    bb->counterI++;
+    return bb->statusI = bb->shouldI;
+  }
+  uint Priority(const bt::Context& ctx) const override {
+    auto bb = std::any_cast<std::shared_ptr<Blackboard>>(ctx.data);
+    return bb->shouldPriorityI;
+  }
+};
